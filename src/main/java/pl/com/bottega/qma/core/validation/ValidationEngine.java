6379643ec3eac1b6c485958a2037cafb3c897d26
Maciej Zubala <maciej.zubala@gmail.com>
2018-04-12 14:46:30 +0200

    validation solved

--- src/main/java/pl/com/bottega/qma/core/validation/ValidationEngine.java
+++ src/main/java/pl/com/bottega/qma/core/validation/ValidationEngine.java
@@ -1,8 +1,25 @@
 package pl.com.bottega.qma.core.validation;
 
-public class ValidationEngine {
+import java.lang.reflect.Field;
 
+public class ValidationEngine {
   public ValidationErrors validate(Object object) {
-    return null;
+    ValidationErrors errors = new ValidationErrors();
+    for(Field field : object.getClass().getDeclaredFields()) {
+      ValidatePresence validatePresence = field.getAnnotation(ValidatePresence.class);
+      if(validatePresence != null)
+        try {
+          handlePresenceValidation(object, field, errors, validatePresence);
+        } catch (IllegalAccessException e) {
+          throw new RuntimeException(e);
+        }
+    }
+    return errors;
+  }
+
+  private void handlePresenceValidation(Object object, Field field, ValidationErrors errors, ValidatePresence validatePresence) throws IllegalAccessException {
+    Object value = field.get(object);
+    if(value == null)
+      errors.addError(field.getName(), "can't be blank");
   }
 }
